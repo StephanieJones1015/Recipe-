6fd4850c39bda5a81b716d464b9e355e99d5c0b1
Stephanie Jones <stephaniewilliams062715@gmail.com>
2025-07-16 17:42:01 -0400

    added js data to give errors or generate the data- tested it in the console and it is populting data with dbmeal

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const SEARCH_API_URL = "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix="; // TODO: Set your API URL here
+const SEARCH_API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s="; // TODO: Set your API URL here
 
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
@@ -8,5 +8,41 @@ const messageArea = document.getElementById("message-area");
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const searchTerm = searchInput.value.trim();
-    console.log("search term: ", searchTerm);
+    
+
+    if (searchTerm) {
+        searchRecipes(searchTerm);
+    } else {
+        showMessage("Please enter an ingredient.", true);
+    } 
 });
+async function searchRecipes(query) {
+   showMessage(`Searching for "${query}"...`, false, true);
+   resultsGrid.innerHTML= ""; 
+
+   try {
+    const response = await fetch(`${SEARCH_API_URL}${query}`);
+    if (!response.ok) throw new Error("Network response was not ok");
+
+    const data = await response.json();
+    clearMessage();
+    console.log("data:", data);
+   } catch (error) {
+    showMessage("OOPS something isn't quite right, Please retry.", true);
+   }
+}
+
+function showMessage(message, isError = false, isLoading = false) {
+    messageArea.textContent = message; 
+    if (isError) messageArea.classList.add("error");
+    if (isLoading) messageArea.classList.add("loading");
+    else {
+    
+    }
+    
+}
+//function to clear the message area
+function clearMessage() {
+    messageArea.textContent = "";
+    messageArea.className = "message";
+}
